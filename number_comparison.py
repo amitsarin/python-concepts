6eba45a77fb2b164147412147bc9d4ac62e1a40b
amitsarin <2711amitkumar@gmail.com>
2020-05-16 14:55:13 +0530

    Add files via upload

---
+++ number_comparison.py
@@ -0,0 +1,10 @@
+num1 = float( input("type first number: "))
+num2 = float( input("type second number: "))
+
+if (num1 > num2):
+    print(num1,"is greater than",num2)
+elif(num1==num2):
+    print(num1, "is equal to", num2)
+else:
+    print(num1, "is less than", num2)
+    
